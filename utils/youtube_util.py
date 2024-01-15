ff85b7179f642fa807fb390b745e2ff3df5a5b0b
Tollef Jørgensen <tollefj@gmail.com>
2024-01-15 11:57:21 +0100

    youtube utils

--- utils/youtube_util.py
+++ utils/youtube_util.py
@@ -4,7 +4,7 @@ import youtube_dl
 def download_video(url, output="store/video.mp4"):
     ydl_opts = {
         # never download anything above 1200 in height, typically 1080p
-        "format": "bestvideo[height<=1200][ext=mp4]+bestaudio[ext=m4a]/mp4",
+        "format": "bestvideo[height<=1200]+bestaudio/best",
         "outtmpl": output,
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
