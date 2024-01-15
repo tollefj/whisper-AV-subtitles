8ceeb0c89d6dc6b3a50d470fa8837ddc1db8239e
Tollef Jørgensen <tollefj@gmail.com>
2024-01-15 11:58:56 +0100

    only install ffmpeg

--- install.sh
+++ install.sh
@@ -15,43 +15,10 @@ detect_os() {
 install_linux() {
     sudo apt update
     sudo apt install ffmpeg
-    # check that cuda 11.8 is installed
-    if ! command -v nvcc &> /dev/null; then
-        echo "CUDA not found. Proceeding with CPU-only installation."
-        # check if conda exists:
-        if ! command -v conda &> /dev/null; then
-            echo "Conda not found. Proceeding with pip installation."
-            pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cpu
-
-        else
-            conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 cpuonly -c pytorch
-        fi
-    else
-        # check if conda exists:
-        if ! command -v conda &> /dev/null; then
-            echo "Conda not found. Proceeding with pip installation."
-            pip install torch==2.0.0+cu113 torchvision==0.15.0+cu113 torchaudio==2.0.0 -f https://download.pytorch.org/whl/torch_stable.html
-        else
-            conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 -c pytorch
-        fi
-    fi
-    if ! command -v conda &> /dev/null; then
-        echo "Conda not found. Proceeding with pip installation."
-        pip install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0
-    else
-        conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 -c pytorch
-    fi
 }
 
 install_mac() {
     brew install ffmpeg
-    # check if conda exists:
-    if ! command -v conda &> /dev/null; then
-        echo "Conda not found. Proceeding with pip installation."
-        pip install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0
-    else
-        conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 -c pytorch
-    fi
 }
 
 install_windows() {
